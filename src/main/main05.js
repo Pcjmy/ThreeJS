a70833ae578cac2855dcc2018f8ece8f9053d6f6
Pcjmy <1716045191@qq.com>
2022-12-20 12:36:37 +0800

    自适应画面

--- src/main/main05.js
+++ src/main/main05.js
@@ -48,6 +48,8 @@ document.body.appendChild(renderer.domElement);
 
 // 创建轨道控制器
 const controls = new OrbitControls(camera, renderer.domElement)
+// 设置控制器阻尼，让控制器更有真实效果，必须在动画循环里调用update()
+controls.enableDamping = true
 
 // 添加坐标轴辅助器
 const axesHelper = new THREE.AxesHelper(5)
@@ -86,9 +88,22 @@ window.addEventListener('dblclick', () => {
 })
 
 function render() {
+  controls.update()
   renderer.render(scence, camera)
   // 渲染下一帧的时候就会调用render函数
   requestAnimationFrame(render)
 }
 
 render()
+
+// 监听画面变化，更新渲染画面
+window.addEventListener('resize', () => {
+  // 更新摄像头
+  camera.aspect = window.innerWidth / window.innerHeight
+  // 更新摄像头的投影矩阵
+  camera.updateProjectionMatrix()
+  // 更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  // 设置渲染器的像素比
+  renderer.setPixelRatio(window.devicePixelRatio)
+})
