7a9d21f857dfcf171809557986f6cbb178ff4560
Pcjmy <1716045191@qq.com>
2022-12-25 14:45:28 +0800

    BufferGeometry

--- src/main/main.js
+++ src/main/main.js
@@ -3,10 +3,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 // 导入动画库
 import gsap from 'gsap'
-// 导入dat.gui
-import * as dat from 'dat.gui'
-
-// 目标：应用图形用户界面更改变量
 
 // 创建场景
 const scence = new THREE.Scene();
@@ -18,40 +14,25 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHei
 camera.position.set(0,0,10);
 scence.add(camera);
 
-// 添加物体
 // 创建几何体
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const geometry = new THREE.BufferGeometry()
+const vertices = new Float32Array([
+  -1.0, -1.0, 1.0,
+  1.0, -1.0, 1.0,
+  1.0, 1.0, 1.0,
+  1.0, 1.0, 1.0,
+  -1.0, 1.0, 1.0,
+  -1.0, -1.0, 1.0
+])
+
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
+
+const Material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
 // 根据几何体和材质创建物体
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-// 修改物体的位置
-// cube.position.set(0, 2.5, 0)
-// cube.position.x = 3
-
-// 缩放
-// cube.scale.set(3, 2, 1)
-// cube.scale.x = 5
-
-// 旋转
-// cube.rotation.set(Math.PI/4, 0, 0)
+const mesh = new THREE.Mesh(geometry, Material)
 
 // 将几何体添加到场景中
-scence.add(cube)
-
-const gui = new dat.GUI()
-gui
-  .add(cube.position, 'x')
-  .min(0)
-  .max(5)
-  .step(0.1)
-  .name('移动X轴')
-  .onChange((value) => {
-    console.log('value=', value)
-  })
-  .onFinishChange((value) => {
-    console.log('finishValue=', value)
-  })
+scence.add(mesh)
 
 const params = {
   color: '#ffff00',
@@ -60,17 +41,6 @@ const params = {
     gsap.to(cube.position, { x: 5, duration: 2, yoyo: true, repeat: -1 })
   }
 }
-gui.addColor(params, 'color').onChange((value) => {
-  console.log('color=', value)
-  cube.material.color.set(value)
-})
-// 设置选项框
-gui.add(cube, 'visible').name('是否显示')
-// 设置按钮点击触发某个事件
-gui.add(params, 'fn').name('立方体运动')
-
-let folder = gui.addFolder('设置立方体')
-folder.add(cube.material, 'wireframe')
 
 // 初始化渲染器
 const renderer = new THREE.WebGLRenderer()
@@ -92,8 +62,6 @@ scence.add(axesHelper)
 
 // 设置时钟
 const clock = new THREE.Clock()
-
-gsap.to(cube.rotation, { x: 2 * Math.PI, duration: 5, ease: 'power1.inOut'})
 
 window.addEventListener('dblclick', () => {
   const fullScreenElement = document.fullscreenElement
