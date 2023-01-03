104d2f9852c9a6b412ccebebff7bc0025dd76896
Pcjmy <1716045191@qq.com>
2023-01-03 16:25:19 +0800

    设置环境纹理

--- src/main/main.js
+++ src/main/main.js
@@ -1,6 +1,19 @@
 import * as THREE from 'three'
 // 导入轨道控制器
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// 导入动画库
+import gsap from 'gsap'
+// 导入dat.gui
+import * as dat from 'dat.gui'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
+
+// 加载hdr环境图
+const rgbeLoader = new RGBELoader();
+rgbeLoader.loadAsync("./hdr/002.hdr").then((texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
+});
 
 // 创建场景
 const scence = new THREE.Scene()
@@ -12,102 +25,30 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHei
 camera.position.set(0,0,10)
 scence.add(camera)
 
-let div = document.createElement('div')
-div.style.width = '200px'
-div.style.height = '200px'
-div.style.position = 'fixed'
-div.style.right = 0
-div.style.top = 0
-div.style.color = '#fff'
-document.body.appendChild(div)
+// 设置cube纹理加载器
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const envMapTexture = cubeTextureLoader.load([
+  "./environmentMaps/1/px.jpg",
+  "./environmentMaps/1/nx.jpg",
+  "./environmentMaps/1/py.jpg",
+  "./environmentMaps/1/ny.jpg",
+  "./environmentMaps/1/pz.jpg",
+  "./environmentMaps/1/nz.jpg",
+]);
 
-let event = {};
-// 单张纹理图的加载
-event.onLoad = function () {
-  console.log("图片加载完成");
-};
-event.onProgress = function (url, num, total) {
-  console.log("图片加载完成:", url);
-  console.log("图片加载进度:", num);
-  console.log("图片总数:", total);
-  let value = ((num / total) * 100).toFixed(2) + "%";
-  console.log("加载进度的百分比：", value);
-  div.innerHTML = value;
-};
-event.onError = function (e) {
-  console.log("图片加载出现错误");
-  console.log(e);
-};
-
-// 设置加载管理器
-const loadingManager = new THREE.LoadingManager(
-  event.onLoad,
-  event.onProgress,
-  event.onError
-);
-// 导入纹理
-const textureLoader = new THREE.TextureLoader(loadingManager);
-const doorColorTexture = textureLoader.load(
-  "./door/color.jpg"
-  //   event.onLoad,
-  //   event.onProgress,
-  //   event.onError
-);
-
-const doorAplhaTexture = textureLoader.load("./door/alpha.jpg");
-const doorAoTexture = textureLoader.load(
-  "./door/ambientOcclusion.jpg"
-);
-//导入置换贴图
-const doorHeightTexture = textureLoader.load("./door/height.jpg");
-// 导入粗糙度贴图
-const roughnessTexture = textureLoader.load("./door/roughness.jpg");
-// 导入金属贴图
-const metalnessTexture = textureLoader.load("./door/metalness.jpg");
-// 导入法线贴图
-const normalTexture = textureLoader.load("./door/normal.jpg");
-
-// 添加物体
-const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1, 100, 100, 100);
-// 材质
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20);
 const material = new THREE.MeshStandardMaterial({
-  color: "#ffff00",
-  map: doorColorTexture,
-  alphaMap: doorAplhaTexture,
-  transparent: true,
-  aoMap: doorAoTexture,
-  aoMapIntensity: 1,
-  displacementMap: doorHeightTexture,
-  displacementScale: 0.1,
-  roughness: 1,
-  roughnessMap: roughnessTexture,
-  metalness: 1,
-  metalnessMap: metalnessTexture,
-  normalMap: normalTexture,
-  //   opacity: 0.3,
-  //   side: THREE.DoubleSide,
+  metalness: 0.7,
+  roughness: 0.1,
+  //   envMap: envMapTexture,
 });
-material.side = THREE.DoubleSide;
-const cube = new THREE.Mesh(cubeGeometry, material);
-scence.add(cube);
-// 给cube添加第二组uv
-cubeGeometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(cubeGeometry.attributes.uv.array, 2)
-);
+const sphere = new THREE.Mesh(sphereGeometry, material);
+scence.add(sphere);
 
-// 添加平面
-const planeGeometry = new THREE.PlaneBufferGeometry(1, 1, 200, 200);
-const plane = new THREE.Mesh(planeGeometry, material);
-plane.position.set(1.5, 0, 0);
-
-scence.add(plane);
-// console.log(plane);
-// 给平面设置第二组uv
-planeGeometry.setAttribute(
-  "uv2",
-  new THREE.BufferAttribute(planeGeometry.attributes.uv.array, 2)
-);
+// 给场景添加背景
+scence.background = envMapTexture;
+// 给场景所有的物体添加默认的环境贴图
+scence.environment = envMapTexture;
 
 // 灯光
 // 环境光
