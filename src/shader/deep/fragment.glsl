390a787ebafe5700c2e4a3f52eed0783c343052d
Pcjmy <1716045191@qq.com>
2023-06-06 15:28:27 +0800

    feat: 条纹相乘

--- src/shader/deep/fragment.glsl
+++ src/shader/deep/fragment.glsl
@@ -5,6 +5,6 @@ void main() {
   // float strength = mod(vUv.y * 5.0, 1.0);
   // strength = step(0.2, strength);
   float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
-  strength += step(0.8, mod(vUv.y * 10.0, 1.0));
+  strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
   gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
