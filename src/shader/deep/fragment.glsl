ba9b54a084644ccf76219d860d6af4f6aab04981
Pcjmy <1716045191@qq.com>
2023-06-06 16:26:25 +0800

    feat: 渐变

--- src/shader/deep/fragment.glsl
+++ src/shader/deep/fragment.glsl
@@ -2,9 +2,9 @@ precision highp float;
 varying vec2 vUv;
 
 void main() {
-  // float strength = mod(vUv.y * 5.0, 1.0);
-  // strength = step(0.2, strength);
-  float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
-  strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
-  gl_FragColor = vec4(strength, strength, strength, 1.0);
+  float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+  float barY = step(0.4, mod(vUv.y * 10.0, 1.0)) * step(0.8, mod(vUv.x * 10.0, 1.0));
+  float strength = barX + barY;
+  gl_FragColor = vec4(vUv, 1, strength);
+  // gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
