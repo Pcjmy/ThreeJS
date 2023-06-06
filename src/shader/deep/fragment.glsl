c3713665abb06d8eb315c44a3981180388482dce
Pcjmy <1716045191@qq.com>
2023-06-06 16:52:38 +0800

    feat: T形图

--- src/shader/deep/fragment.glsl
+++ src/shader/deep/fragment.glsl
@@ -3,7 +3,7 @@ uniform float uTime;
 varying vec2 vUv;
 
 void main() {
-  float barX = step(0.4, mod((vUv.x + uTime * 0.1) * 10.0, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
+  float barX = step(0.4, mod(vUv.x * 10.0 - 0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
   float barY = step(0.4, mod(vUv.y * 10.0, 1.0)) * step(0.8, mod(vUv.x * 10.0, 1.0));
   float strength = barX + barY;
   gl_FragColor = vec4(vUv, 1, strength);
