e92a5e9157293738c8ab5fae22dc98e5f5b4c06c
Pcjmy <1716045191@qq.com>
2023-06-06 12:07:54 +0800

    feat: 网格

--- src/shader/deep/fragment.glsl
+++ src/shader/deep/fragment.glsl
@@ -2,7 +2,9 @@ precision highp float;
 varying vec2 vUv;
 
 void main() {
-  float strength = mod(vUv.y * 5.0, 1.0);
-  strength = step(0.2, strength);
+  // float strength = mod(vUv.y * 5.0, 1.0);
+  // strength = step(0.2, strength);
+  float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+  strength += step(0.8, mod(vUv.y * 10.0, 1.0));
   gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
