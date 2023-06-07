f6e742d5da102a067dbdaa5eb64e1c2c2073bce3
Pcjmy <1716045191@qq.com>
2023-06-07 10:08:42 +0800

    feat: 渐变格子

--- src/shader/deep/fragment.glsl
+++ src/shader/deep/fragment.glsl
@@ -3,6 +3,6 @@ uniform float uTime;
 varying vec2 vUv;
 
 void main() {
-  float strength = ceil(vUv.y * 10.0) / 10.0;
+  float strength = ceil(vUv.y * 10.0) / 10.0 * ceil(vUv.x * 10.0) / 10.0;
   gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
