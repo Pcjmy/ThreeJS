e41cd2521b820378940dfca1279288dc4247636e
Pcjmy <1716045191@qq.com>
2023-06-07 22:13:03 +0800

    feat: 噪声函数

--- src/shader/deep/fragment.glsl
+++ src/shader/deep/fragment.glsl
@@ -19,8 +19,28 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid)
   );
 }
 
+// 噪声函数
+float noise (in vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    // Smooth Interpolation
+
+    // Cubic Hermine Curve.  Same as SmoothStep()
+    vec2 u = f*f*(3.0-2.0*f);
+    // u = smoothstep(0.,1.,f);
+
+    // Mix 4 coorners percentages
+    return mix(a, b, u.x) + (c - a)* u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
+
 void main() {
-  float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (PI * 2.0);
-  float strength = sin(angle * 100.0);
+  float strength = noise(vUv);
   gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
