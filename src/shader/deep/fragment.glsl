41a20e992d2b22e72d635097456b9ca5cc1f6650
Pcjmy <1716045191@qq.com>
2023-06-07 23:02:29 +0800

    feat: 造型函数

--- src/shader/deep/fragment.glsl
+++ src/shader/deep/fragment.glsl
@@ -1,5 +1,6 @@
 precision highp float;
 uniform float uTime;
+uniform vec2 u_resolution;
 varying vec2 vUv;
 
 #define PI 3.1415926535
@@ -83,7 +84,15 @@ float cnoise(vec2 P)
   return 2.3 * n_xy;
 }
 
+float plot(vec2 st, float pct){
+  return smoothstep( pct-0.02, pct, st.y) - smoothstep( pct, pct+0.02, st.y);
+}
+
 void main() {
-  float strength = step(0.5, cnoise(vUv * 100.0));
-  gl_FragColor = vec4(strength, strength, strength, 1.0);
+  vec2 st = gl_FragCoord.xy/u_resolution;
+  float y = pow(st.x,5.0);
+  vec3 color = vec3(y);
+  float pct = plot(st,y);
+  color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);
+  gl_FragColor = vec4(color,1.0);
 }
