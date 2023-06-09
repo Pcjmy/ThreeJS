e166913c354632e30e1add00e14e9f68f2a85688
Pcjmy <1716045191@qq.com>
2023-06-09 10:50:20 +0800

    feat: 孔明灯

--- src/shader/flylight/fragment.glsl
+++ src/shader/flylight/fragment.glsl
@@ -4,7 +4,11 @@ varying vec4 gPosition;
 
 void main() {
   vec4 redColor = vec4(1, 0, 0, 1);
-  vec4 yellowColor = vec4(1, 1, 0, 1);
+  vec4 yellowColor = vec4(1, 1, 0.5, 1);
   vec4 mixColor = mix(yellowColor, redColor, gPosition.y / 3.0);
-  gl_FragColor = vec4(mixColor.xyz, 1.0);
+  if (gl_FrontFacing) {
+    gl_FragColor = vec4(mixColor.xyz - vPosition.y / 100.0 - 0.5, 1.0);
+  } else {
+    gl_FragColor = vec4(mixColor.xyz, 1.0);
+  }
 }
