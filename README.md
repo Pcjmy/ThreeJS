014486feb35e2210d6f2be4221c17e856c1f1b14
Pcjmy <1716045191@qq.com>
2023-06-11 12:23:21 +0800

    feat: 官网水模型

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 ## README
 
-![](assets/1.png)
-
-![](assets/2.png)
-
-![](assets/3.png)
-
-![](assets/4.png)
-
-![](assets/5.png)
+Three.js
 
 ### 项目搭建
 
--- src/main/main.js
+++ src/main/main.js
@@ -5,11 +5,7 @@ import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
 // 导入dat.gui
 import * as dat from 'dat.gui'
 import gsap from 'gsap'
-
-// 顶点着色器
-import vertexShader from '../shader/flylight/vertex.glsl'
-// 片元着色器
-import fragmentShader from '../shader/flylight/fragment.glsl'
+import { Water } from 'three/examples/jsm/objects/Water2'
 
 const gui = new dat.GUI()
 
@@ -34,116 +30,16 @@ scene.add(camera)
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
 
-const params = {
-  uWaresFrequency: 14,
-  uScale: 0.03,
-  uXzScale: 1.5,
-  uNoiseFrequency: 10,
-  uNoiseScale: 1.5,
-  uLowColor: '#ff0000',
-  uHighColor: '#ffff00',
-  uXspeed: 1,
-  uZspeed: 1,
-  uNoiseSpeed: 1,
-  uOpacity: 1
-}
-
-// 创建着色器材质
-const shaderMaterial = new THREE.ShaderMaterial({
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader,
-  side: THREE.DoubleSide,
-  uniforms: {
-    uWaresFrequency: {
-      value: params.uWaresFrequency
-    },
-    uScale: {
-      value: params.uScale
-    },
-    uXzScale: {
-      value: params.uXzScale
-    },
-    uNoiseFrequency: {
-      value: params.uNoiseFrequency
-    },
-    uNoiseScale: {
-      value: params.uNoiseScale
-    },
-    uTime: {
-      value: params.uTime
-    },
-    uLowColor: {
-      value: new THREE.Color(params.uLowColor)
-    },
-    uHighColor: {
-      value: new THREE.Color(params.uHighColor)
-    },
-    uXspeed: {
-      value: params.uXspeed
-    },
-    uZspeed: {
-      value: params.uZspeed
-    },
-    uNoiseSpeed: {
-      value: params.uNoiseSpeed
-    },
-    uOpacity: {
-      value: params.uOpacity
-    }
-  },
-  transparent: true
+const water = new Water(new THREE.PlaneGeometry(1, 1, 1024, 1024), {
+  color: '#ffffff',
+  scale: 1,
+  flowDirection: new THREE.Vector2(1, 1),
+  textureHeight: 1024,
+  textureWidth: 1024
 })
+water.rotation.x = -Math.PI / 2
 
-gui.add(params, 'uWaresFrequency').min(1).max(100).step(0.1).onChange((value) => {
-  shaderMaterial.uniforms.uWaresFrequency.value = value
-})
-
-gui.add(params, 'uScale').min(0).max(0.2).step(0.001).onChange((value) => {
-  shaderMaterial.uniforms.uScale.value = value
-})
-
-gui.add(params, 'uXzScale').min(0).max(5).step(0.1).onChange((value) => {
-  shaderMaterial.uniforms.uXzScale.value = value
-})
-
-gui.add(params, 'uNoiseFrequency').min(0).max(100).step(0.1).onChange((value) => {
-  shaderMaterial.uniforms.uNoiseFrequency.value = value
-})
-
-gui.add(params, 'uNoiseScale').min(0).max(5).step(0.01).onChange((value) => {
-  shaderMaterial.uniforms.uNoiseScale.value = value
-})
-
-gui.addColor(params, 'uLowColor').onFinishChange((value) => {
-  shaderMaterial.uniforms.uLowColor.value = new THREE.Color(value)
-})
-
-gui.addColor(params, 'uHighColor').onFinishChange((value) => {
-  shaderMaterial.uniforms.uHighColor.value = new THREE.Color(value)
-})
-
-gui.add(params, 'uXspeed').min(0).max(5).step(0.01).onChange((value) => {
-  shaderMaterial.uniforms.uXspeed.value = value
-})
-
-gui.add(params, 'uZspeed').min(0).max(5).step(0.01).onChange((value) => {
-  shaderMaterial.uniforms.uZspeed.value = value
-})
-
-gui.add(params, 'uNoiseSpeed').min(0).max(5).step(0.01).onChange((value) => {
-  shaderMaterial.uniforms.uNoiseSpeed.value = value
-})
-
-gui.add(params, 'uOpacity').min(0).max(1).step(0.01).onChange((value) => {
-  shaderMaterial.uniforms.uOpacity.value = value
-})
-
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(1, 1, 1024, 1024),
-  shaderMaterial
-)
-plane.rotation.x = -Math.PI / 2
-scene.add(plane)
+scene.add(water)
 
 // 初始化渲染器
 const renderer = new THREE.WebGLRenderer()
@@ -159,9 +55,6 @@ const controls = new OrbitControls(camera, renderer.domElement)
 // 设置时钟
 const clock = new THREE.Clock()
 function animate(t) {
-  controls.update()
-  const elapsedTime = clock.getElapsedTime()
-  shaderMaterial.uniforms.uTime.value = elapsedTime
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
 }
