fddb108d5a838fd54d3df001fa4eafb554e06466
Pcjmy <1716045191@qq.com>
2025-04-20 22:16:48 +0800

    feat: 根据视图Position设置点图案远近大小

--- src/shader/basic/vertexShader.glsl
+++ src/shader/basic/vertexShader.glsl
@@ -1,6 +1,7 @@
 varying vec2 vUv;
 
 attribute float imgIndex;
+attribute float aScale;
 varying float vImgIndex;
 
 void main() {
@@ -8,8 +9,8 @@ void main() {
   vec4 viewPosition = viewMatrix * modelPosition;
   gl_Position = projectionMatrix * viewPosition;
 
-  // 设置点的大小
-  gl_PointSize = 100.0;
+  // 根据viewPosition的z坐标决定是否远离摄像机
+  gl_PointSize = 200.0 * aScale / -viewPosition.z;
   vUv = uv;
   vImgIndex = imgIndex;
 }
