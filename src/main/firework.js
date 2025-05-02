99c540d2e33cd2016a42d9181b7a54658c5a0e25
Pcjmy <1716045191@qq.com>
2025-05-03 00:02:10 +0800

    feat: prettier

--- src/main/firework.js
+++ src/main/firework.js
@@ -5,190 +5,190 @@ import fireWorksVertex from '../shader/fireworks/vertex.glsl'
 import fireWorksFragment from '../shader/fireworks/fragment.glsl'
 
 export default class FireWorks {
-  constructor(color, to, from = { x: 0, y: 0, z: 0 }) {
-    console.log('创建烟花：', color, to)
-    this.color = new THREE.Color(color)
+	constructor(color, to, from = { x: 0, y: 0, z: 0 }) {
+		console.log('创建烟花：', color, to)
+		this.color = new THREE.Color(color)
 
-    // 创建烟花
-    this.startGeometry = new THREE.BufferGeometry()
-    const startPositionArray = new Float32Array(3)
-    startPositionArray[0] = from.x
-    startPositionArray[1] = from.y
-    startPositionArray[2] = from.z
+		// 创建烟花
+		this.startGeometry = new THREE.BufferGeometry()
+		const startPositionArray = new Float32Array(3)
+		startPositionArray[0] = from.x
+		startPositionArray[1] = from.y
+		startPositionArray[2] = from.z
 
-    this.startGeometry.setAttribute(
-      'position',
-      new THREE.BufferAttribute(startPositionArray, 3)
-    );
+		this.startGeometry.setAttribute(
+			'position',
+			new THREE.BufferAttribute(startPositionArray, 3)
+		)
 
-    const astepArray = new Float32Array(3)
-    astepArray[0] = to.x - from.x
-    astepArray[1] = to.y - from.y
-    astepArray[2] = to.z - from.z
-    this.startGeometry.setAttribute(
-      'aStep',
-      new THREE.BufferAttribute(astepArray, 3)
-    )
+		const astepArray = new Float32Array(3)
+		astepArray[0] = to.x - from.x
+		astepArray[1] = to.y - from.y
+		astepArray[2] = to.z - from.z
+		this.startGeometry.setAttribute(
+			'aStep',
+			new THREE.BufferAttribute(astepArray, 3)
+		)
 
-    // 设置着色器材质
-    this.startMaterial = new THREE.ShaderMaterial({
-      vertexShader: startPointVertex,
-      fragmentShader: startPointFragment,
-      transparent: true,
-      blending: THREE.AdditiveBlending,
-      depthWrite: false,
-      uniforms: {
-        uTime: {
-          value: 0
-        },
-        uSize: {
-          value: 20
-        },
-        uColor: {
-          value: this.color
-        }
-      }
-    })
+		// 设置着色器材质
+		this.startMaterial = new THREE.ShaderMaterial({
+			vertexShader: startPointVertex,
+			fragmentShader: startPointFragment,
+			transparent: true,
+			blending: THREE.AdditiveBlending,
+			depthWrite: false,
+			uniforms: {
+				uTime: {
+					value: 0
+				},
+				uSize: {
+					value: 20
+				},
+				uColor: {
+					value: this.color
+				}
+			}
+		})
 
-    console.log('this.startMaterial=', this.startMaterial)
+		console.log('this.startMaterial=', this.startMaterial)
 
-    // 创建烟花点球
-    this.startPoint = new THREE.Points(this.startGeometry, this.startMaterial)
+		// 创建烟花点球
+		this.startPoint = new THREE.Points(this.startGeometry, this.startMaterial)
 
-    // 开始计时
-    this.clock = new THREE.Clock()
+		// 开始计时
+		this.clock = new THREE.Clock()
 
-    // 创建爆炸的烟花
-    this.fireWorksGeometry = new THREE.BufferGeometry()
-    this.fireWorksCount = 180 + Math.floor(Math.random() * 180)
-    const positionFireWorksArray = new Float32Array(this.fireWorksCount * 3)
-    const scaleFireWorksArray = new Float32Array(this.fireWorksCount)
-    const directionArray = new Float32Array(this.fireWorksCount * 3)
+		// 创建爆炸的烟花
+		this.fireWorksGeometry = new THREE.BufferGeometry()
+		this.fireWorksCount = 180 + Math.floor(Math.random() * 180)
+		const positionFireWorksArray = new Float32Array(this.fireWorksCount * 3)
+		const scaleFireWorksArray = new Float32Array(this.fireWorksCount)
+		const directionArray = new Float32Array(this.fireWorksCount * 3)
 
-    for (let i = 0; i < this.fireWorksCount; i++) {
-      // 一开始烟花的位置
-      positionFireWorksArray[i * 3 + 0] = to.x
-      positionFireWorksArray[i * 3 + 1] = to.y
-      positionFireWorksArray[i * 3 + 2] = to.z
+		for (let i = 0; i < this.fireWorksCount; i++) {
+			// 一开始烟花的位置
+			positionFireWorksArray[i * 3 + 0] = to.x
+			positionFireWorksArray[i * 3 + 1] = to.y
+			positionFireWorksArray[i * 3 + 2] = to.z
 
-      // 设置烟花所有粒子初始化大小
-      scaleFireWorksArray[i] = Math.random()
+			// 设置烟花所有粒子初始化大小
+			scaleFireWorksArray[i] = Math.random()
 
-      // 设置四周发射的角度
-      let theta = Math.random() * Math.PI * 2
-      let beta = Math.random() * Math.PI * 2
-      let radius = Math.random()
+			// 设置四周发射的角度
+			let theta = Math.random() * Math.PI * 2
+			let beta = Math.random() * Math.PI * 2
+			let radius = Math.random()
 
-      directionArray[i * 3 + 0] = radius * Math.sin(theta) + radius * Math.sin(beta)
-      directionArray[i * 3 + 1] = radius * Math.cos(theta) + radius * Math.cos(beta)
-      directionArray[i * 3 + 2] = radius * Math.sin(theta) + radius * Math.cos(beta)
-    }
-    this.fireWorksGeometry.setAttribute(
-      'position',
-      new THREE.BufferAttribute(positionFireWorksArray, 3)
-    )
+			directionArray[i * 3 + 0] =
+				radius * Math.sin(theta) + radius * Math.sin(beta)
+			directionArray[i * 3 + 1] =
+				radius * Math.cos(theta) + radius * Math.cos(beta)
+			directionArray[i * 3 + 2] =
+				radius * Math.sin(theta) + radius * Math.cos(beta)
+		}
+		this.fireWorksGeometry.setAttribute(
+			'position',
+			new THREE.BufferAttribute(positionFireWorksArray, 3)
+		)
 
-    this.fireWorksGeometry.setAttribute(
-      'aScale',
-      new THREE.BufferAttribute(scaleFireWorksArray, 1)
-    )
+		this.fireWorksGeometry.setAttribute(
+			'aScale',
+			new THREE.BufferAttribute(scaleFireWorksArray, 1)
+		)
 
-    this.fireWorksGeometry.setAttribute(
-      'aRandom',
-      new THREE.BufferAttribute(directionArray, 3) 
-    )
+		this.fireWorksGeometry.setAttribute(
+			'aRandom',
+			new THREE.BufferAttribute(directionArray, 3)
+		)
 
-    this.fireWorkMaterial = new THREE.ShaderMaterial({
-      uniforms: {
-        uTime: {
-          value: 0
-        },
-        uSize: {
-          value: 0
-        },
-        uColor: {
-          value: this.color
-        }
-      },
-      vertexShader: fireWorksVertex,
-      fragmentShader: fireWorksFragment,
-      transparent: true,
-      blending: THREE.AdditiveBlending,
-      depthWrite: false,
-    })
+		this.fireWorkMaterial = new THREE.ShaderMaterial({
+			uniforms: {
+				uTime: {
+					value: 0
+				},
+				uSize: {
+					value: 0
+				},
+				uColor: {
+					value: this.color
+				}
+			},
+			vertexShader: fireWorksVertex,
+			fragmentShader: fireWorksFragment,
+			transparent: true,
+			blending: THREE.AdditiveBlending,
+			depthWrite: false
+		})
 
-    this.fireWorks = new THREE.Points(
-      this.fireWorksGeometry,
-      this.fireWorkMaterial
-    )
+		this.fireWorks = new THREE.Points(
+			this.fireWorksGeometry,
+			this.fireWorkMaterial
+		)
 
-    // 创建音频
-    this.listener = new THREE.AudioListener()
-    this.sound = new THREE.Audio(this.listener)
-    this.sendSound = new THREE.Audio(this.listener)
+		// 创建音频
+		this.listener = new THREE.AudioListener()
+		this.sound = new THREE.Audio(this.listener)
+		this.sendSound = new THREE.Audio(this.listener)
 
-    // 创建音频加载器
-    const audioLoader = new THREE.AudioLoader()
-    audioLoader.load(
-      `./assets/audio/pow${Math.floor(Math.random() * 4 + 1)}.ogg`,
-      (buffer) => {
-        this.sound.setBuffer(buffer)
-        this.sound.setLoop(false)
-        this.sound.setVolume(1)
-      },
-    )
+		// 创建音频加载器
+		const audioLoader = new THREE.AudioLoader()
+		audioLoader.load(
+			`./assets/audio/pow${Math.floor(Math.random() * 4 + 1)}.ogg`,
+			(buffer) => {
+				this.sound.setBuffer(buffer)
+				this.sound.setLoop(false)
+				this.sound.setVolume(1)
+			}
+		)
 
-    audioLoader.load(
-      `./assets/audio/send.mp3`,
-      (buffer) => {
-        this.sendSound.setBuffer(buffer)
-        this.sendSound.setLoop(false)
-        this.sendSound.setVolume(1)
-      },
-    )
-  }
+		audioLoader.load(`./assets/audio/send.mp3`, (buffer) => {
+			this.sendSound.setBuffer(buffer)
+			this.sendSound.setLoop(false)
+			this.sendSound.setVolume(1)
+		})
+	}
 
-  // 添加到场景
-  addScene(scene, camera) {
-    scene.add(this.startPoint)
-    scene.add(this.fireWorks)
-    this.scene = scene
-  }
+	// 添加到场景
+	addScene(scene, camera) {
+		scene.add(this.startPoint)
+		scene.add(this.fireWorks)
+		this.scene = scene
+	}
 
-  update() {
-    const elapsedTime = this.clock.getElapsedTime()
-    if (elapsedTime > 0.2 && elapsedTime < 1) {
-      if (!this.sendSound.isPlaying && !this.sendSoundPlay) {
-        this.sendSound.play()
-        this.sendSoundPlay = true
-      }
-      this.startMaterial.uniforms.uTime.value = elapsedTime
-      this.startMaterial.uniforms.uSize.value = 20
-    } else if (elapsedTime > 1) {
-      const time = elapsedTime - 1
-      this.startMaterial.uniforms.uSize.value = 0
-      this.startPoint.clear()
-      this.startGeometry.dispose()
-      this.startMaterial.dispose()
+	update() {
+		const elapsedTime = this.clock.getElapsedTime()
+		if (elapsedTime > 0.2 && elapsedTime < 1) {
+			if (!this.sendSound.isPlaying && !this.sendSoundPlay) {
+				this.sendSound.play()
+				this.sendSoundPlay = true
+			}
+			this.startMaterial.uniforms.uTime.value = elapsedTime
+			this.startMaterial.uniforms.uSize.value = 20
+		} else if (elapsedTime > 1) {
+			const time = elapsedTime - 1
+			this.startMaterial.uniforms.uSize.value = 0
+			this.startPoint.clear()
+			this.startGeometry.dispose()
+			this.startMaterial.dispose()
 
-      if (!this.sound.isPlaying && !this.play) {
-        this.sound.play()
-        this.play = true
-      }
+			if (!this.sound.isPlaying && !this.play) {
+				this.sound.play()
+				this.play = true
+			}
 
-      // 设置烟花显示
-      this.fireWorkMaterial.uniforms.uSize.value = 20
-      this.fireWorkMaterial.uniforms.uTime.value = time
+			// 设置烟花显示
+			this.fireWorkMaterial.uniforms.uSize.value = 20
+			this.fireWorkMaterial.uniforms.uTime.value = time
 
-      if (time > 3) {
-        this.scene.remove(this.fireWorks)
-        this.scene.remove(this.startPoint)
-        this.fireWorks.clear()
-        this.fireWorksGeometry.dispose()
-        this.fireWorkMaterial.dispose()
-        return true
-      }
-      return false
-    }
-  }
+			if (time > 3) {
+				this.scene.remove(this.fireWorks)
+				this.scene.remove(this.startPoint)
+				this.fireWorks.clear()
+				this.fireWorksGeometry.dispose()
+				this.fireWorkMaterial.dispose()
+				return true
+			}
+			return false
+		}
+	}
 }
--- src/main/main.js
+++ src/main/main.js
@@ -23,10 +23,10 @@ const scene = new THREE.Scene()
 
 // 创建透视相机
 const camera = new THREE.PerspectiveCamera(
-  90,
-  window.innerWidth/window.innerHeight,
-  0.1,
-  1000
+	90,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
 )
 
 // 设置相机位置
@@ -38,9 +38,9 @@ scene.add(camera)
 // 创建纹理加载器对象
 const rgbeLoader = new RGBELoader()
 rgbeLoader.loadAsync('./public/assets/2k.hdr').then((texture) => {
-  texture.mapping = THREE.EquirectangularReflectionMapping
-  scene.background = texture
-  scene.environment = texture
+	texture.mapping = THREE.EquirectangularReflectionMapping
+	scene.background = texture
+	scene.environment = texture
 })
 
 // 添加坐标轴辅助器
@@ -49,10 +49,10 @@ rgbeLoader.loadAsync('./public/assets/2k.hdr').then((texture) => {
 
 // 创建着色器材质
 const shaderMaterial = new THREE.ShaderMaterial({
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader,
-  side: THREE.DoubleSide,
-  // transparent: true
+	vertexShader: vertexShader,
+	fragmentShader: fragmentShader,
+	side: THREE.DoubleSide
+	// transparent: true
 })
 
 // 创建平面
@@ -72,48 +72,48 @@ renderer.toneMappingExposure = 0.2
 const gltfLoader = new GLTFLoader()
 
 gltfLoader.load('./public/assets/model/newyears_min.glb', (gltf) => {
-  scene.add(gltf.scene)
+	scene.add(gltf.scene)
 
-  // 创建水面
-  const waterGeometry = new THREE.PlaneGeometry(50, 50)
-  const water = new Water(waterGeometry, {
-    scale: 4,
-    textureWidth: 1024,
-    textureHeight: 1024, 
-  })
+	// 创建水面
+	const waterGeometry = new THREE.PlaneGeometry(50, 50)
+	const water = new Water(waterGeometry, {
+		scale: 4,
+		textureWidth: 1024,
+		textureHeight: 1024
+	})
 
-  water.position.y = 1;
-  water.rotation.x = -Math.PI / 2
+	water.position.y = 1
+	water.rotation.x = -Math.PI / 2
 
-  scene.add(water)
+	scene.add(water)
 })
 
 let LightBox = null
 gltfLoader.load('./public/assets/model/flyLight.glb', (gltf) => {
-  // scene.add(gltf.scene)
-  LightBox = gltf.scene.children[0]
-  LightBox.material = shaderMaterial
+	// scene.add(gltf.scene)
+	LightBox = gltf.scene.children[0]
+	LightBox.material = shaderMaterial
 
-  for(let i = 0; i < 150 ; i++) {
-    let flyLight = gltf.scene.clone(true)
-    let x = (Math.random() - 0.5) * 300
-    let z = (Math.random() - 0.5) * 300
-    let y = Math.random() * 60 + 5
-    flyLight.position.set(x, y, z)
-    gsap.to(flyLight.rotation, {
-      y: 2 * Math.PI,
-      duration: 10 + Math.random() * 10,
-      repeat: -1
-    })
-    gsap.to(flyLight.position, {
-      x: '+=' + Math.random() * 5,
-      y: '+=' + Math.random() * 20,
-      yoyo: true,
-      duration: 5 + Math.random() * 10,
-      repeat: -1
-    })
-    scene.add(flyLight)
-  }
+	for (let i = 0; i < 150; i++) {
+		let flyLight = gltf.scene.clone(true)
+		let x = (Math.random() - 0.5) * 300
+		let z = (Math.random() - 0.5) * 300
+		let y = Math.random() * 60 + 5
+		flyLight.position.set(x, y, z)
+		gsap.to(flyLight.rotation, {
+			y: 2 * Math.PI,
+			duration: 10 + Math.random() * 10,
+			repeat: -1
+		})
+		gsap.to(flyLight.position, {
+			x: '+=' + Math.random() * 5,
+			y: '+=' + Math.random() * 20,
+			yoyo: true,
+			duration: 5 + Math.random() * 10,
+			repeat: -1
+		})
+		scene.add(flyLight)
+	}
 })
 
 // 设置渲染尺寸大小
@@ -133,14 +133,14 @@ controls.autoRotateSpeed = 0.1
 
 // 监听画面变化，更新渲染画面
 window.addEventListener('resize', () => {
-  // 更新摄像头
-  camera.aspect = window.innerWidth / window.innerHeight
-  // 更新摄像头的投影矩阵
-  camera.updateProjectionMatrix()
-  // 更新渲染器
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  // 设置渲染器的像素比
-  renderer.setPixelRatio(window.devicePixelRatio)
+	// 更新摄像头
+	camera.aspect = window.innerWidth / window.innerHeight
+	// 更新摄像头的投影矩阵
+	camera.updateProjectionMatrix()
+	// 更新渲染器
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	// 设置渲染器的像素比
+	renderer.setPixelRatio(window.devicePixelRatio)
 })
 
 // 管理烟花
@@ -148,37 +148,37 @@ let fireworks = []
 
 // 设置创建烟花函数
 let createFireWorks = () => {
-  let color = `hsl(${Math.floor(Math.random() * 360)}, 100%, 80%)`
-  let position = {
-    x: (Math.random() - 0.5) * 40,
-    z: -(Math.random() - 0.5) * 40,
-    y: 7 + Math.random() * 25,
-  }
+	let color = `hsl(${Math.floor(Math.random() * 360)}, 100%, 80%)`
+	let position = {
+		x: (Math.random() - 0.5) * 40,
+		z: -(Math.random() - 0.5) * 40,
+		y: 7 + Math.random() * 25
+	}
 
-  // 随机生成颜色和烟花放的位置
-  let firework = new FireWorks(color, position)
-  firework.addScene(scene, camera)
-  fireworks.push(firework)
-};
+	// 随机生成颜色和烟花放的位置
+	let firework = new FireWorks(color, position)
+	firework.addScene(scene, camera)
+	fireworks.push(firework)
+}
 
 window.addEventListener('click', createFireWorks)
 
 // 设置时钟
 const clock = new THREE.Clock()
 function animate(t) {
-  controls.update()
+	controls.update()
 
-  // 反向遍历避免索引错乱
-  for (let i = fireworks.length - 1; i >= 0; i--) {
-    const firework = fireworks[i]
-    const shouldRemove = firework.update()
-    if (shouldRemove) {
-      fireworks.splice(i, 1)
-    }
-  }
+	// 反向遍历避免索引错乱
+	for (let i = fireworks.length - 1; i >= 0; i--) {
+		const firework = fireworks[i]
+		const shouldRemove = firework.update()
+		if (shouldRemove) {
+			fireworks.splice(i, 1)
+		}
+	}
 
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
 }
 
 animate()
