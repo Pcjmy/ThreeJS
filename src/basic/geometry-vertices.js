c6f178e580464a8999ba735057f0d5352776c30d
Pcjmy <1716045191@qq.com>
2025-12-14 20:51:15 +0800

    feat: 基础几何顶点渲染

---
+++ src/basic/geometry-vertices.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+	45,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+)
+
+const geometry = new THREE.BufferGeometry()
+
+const vertices = new Float32Array([
+	-1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 1.0, 1.0, 0.0
+])
+
+geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
+
+const material = new THREE.MeshBasicMaterial({
+	color: 0x00ff00,
+	side: THREE.DoubleSide
+})
+const plane = new THREE.Mesh(geometry, material)
+scene.add(plane)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+camera.position.z = 5
+camera.position.y = 3
+camera.position.x = 2
+camera.lookAt(0, 0, 0)
+
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+// 添加轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+// 开启阻尼效果
+controls.enableDamping = true
+// 设置阻尼系数
+controls.dampingFactor = 0.05
+// 开启自动旋转
+// controls.autoRotate = true
+// 设置自动旋转速度
+// controls.autoRotateSpeed = 0.5
+
+function animate() {
+	controls.update()
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+}
+
+animate()
+
+window.addEventListener('resize', () => {
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+})
