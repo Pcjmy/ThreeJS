47dee85b72e0e8f8819e496db4a0d0f3984a98a9
Pcjmy <1716045191@qq.com>
2025-12-22 00:06:03 +0800

    feat: GLTF模型加载

---
+++ src/basic/gltf-model.js
@@ -0,0 +1,75 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+	45,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+camera.position.z = 5
+camera.position.y = 3
+camera.position.x = 2
+camera.lookAt(0, 0, 0)
+
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
+// 添加轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+// 开启阻尼效果
+controls.enableDamping = true
+// 设置阻尼系数
+controls.dampingFactor = 0.05
+// 开启自动旋转
+// controls.autoRotate = true
+// 设置自动旋转速度
+// controls.autoRotateSpeed = 0.5
+
+function animate() {
+	controls.update()
+	requestAnimationFrame(animate)
+	renderer.render(scene, camera)
+}
+
+animate()
+
+window.addEventListener('resize', () => {
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+})
+
+const gui = new GUI()
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 100)
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+
+const box = new THREE.Mesh(boxGeometry, boxMaterial)
+// scene.add(box)
+
+// scene.fog = new THREE.Fog(0x999999, 0.1, 50)
+
+scene.fog = new THREE.FogExp2(0x999999, 0.1)
+scene.background = new THREE.Color(0x999999)
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.load('./model/duck.glb', (gltf) => {
+	scene.add(gltf.scene)
+})
+
+const rgbeLoader = new RGBELoader()
+rgbeLoader.load('./public/hdr/Alex_Hart-Nature_Lab_Bones_2k.hdr', (texture) => {
+	texture.mapping = THREE.EquirectangularReflectionMapping
+	scene.environment = texture
+})
